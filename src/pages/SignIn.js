7d75b872afd5db249f62944d3ad6b6c9b96c7702
Amkam <amaduskamara@gmail.com>
2022-10-28 11:54:32 +0000

    Add form for register and login

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
-  )
-}
+    <section className="login-signup flex justify-between items-center">
+      <div className="w-1/2"></div>
+      <div className="w-1/2">
+        <h1 className="text-3xl font-semibold text-gray-600">
+          Start Entrepreneurship <br /> Today{" "}
+        </h1>
+      </div>
+    </section>
+  );
+};
 
-export default SignIn
+export default SignIn;
