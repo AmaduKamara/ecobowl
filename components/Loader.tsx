b78213bcf8f8fde908a1901fe99fb0f37fb7a7ba
Ishmael Kargbo <ishmaelkargbo5@outlook.com>
2022-10-28 12:49:57 +0000

    Create Loader.tsx

---
+++ components/Loader.tsx
@@ -0,0 +1,12 @@
+export const Loader = () => {
+    return (
+        <div className="flex flex-col items-center">
+            <div className="cubeLoader">
+                <div className="cube load"></div>
+                <div className="cube load"></div>
+                <div className="cube load"></div>
+                <div className="cube load"></div>
+            </div>
+        </div>
+    )
+}
