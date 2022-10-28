9a5c0fa44a3c4e17bf5147644463581ee2cd3d89
Ishmael Kargbo <ishmaelkargbo5@outlook.com>
2022-10-28 12:50:28 +0000

    Create RadioField.tsx

---
+++ components/ui/RadioField.tsx
@@ -0,0 +1,24 @@
+import { Radio } from "antd";
+
+export const RadioField = (props) => {
+    const {
+        label = "",
+        error = "",
+        name = "",
+        value = "",
+        required = false,
+        id = "",
+        children,
+        onChange = (e?: any) => { }
+    } = props
+
+    return (
+        <>
+            <label className={`text-sm block mb-1.5 ${error ? 'text-red-600' : ''}`} htmlFor={id}>{label}{required ? <span className="text-red-600">*</span> : ""}:</label>
+            <Radio.Group onChange={onChange} name={name} value={value} size="large">{children}</Radio.Group>
+            {
+                error ? <p className="text-xs mt-1 text-red-600">{error}</p> : null
+            }
+        </>
+    )
+}
