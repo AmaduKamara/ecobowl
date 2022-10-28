b82cb99abdb8563d7cfbc2defc76133dc10203ec
Amkam <amaduskamara@gmail.com>
2022-10-28 15:58:03 +0000

    Edit and change icons for dashboard

--- components/chart/count.tsx
+++ components/chart/count.tsx
@@ -1,6 +1,10 @@
 import { FaCashRegister, FaRegIdBadge } from "react-icons/fa";
 import { BiCollection } from "react-icons/bi";
 import { MdBatchPrediction } from "react-icons/md";
+import { FcBusiness } from "react-icons/fc";
+import { IoIosPeople } from "react-icons/io";
+import { MdOutlineEventSeat } from "react-icons/md";
+import { FaSchool } from "react-icons/fa";
 import { Card } from "../AppLayout";
 import Router from "next/router";
 
@@ -13,10 +17,10 @@ export const CountChat = ({ record }) => {
                         <Card onClick={() => Router.push('/product')} className="lg:p-10 md:px-8 pc:p-4 p-6 flex justify-between items-center">
                             <div>
                                 <p className='text-3xl font-bold mb-2'>{record.sales}</p>
-                                <p className='text-base'>Total Sales</p>
+                                <p className='text-base'>Business</p>
                             </div>
                             <div>
-                                <FaCashRegister className="md:text-5xl text-3xl text-green-500" />
+                                <FcBusiness className="md:text-5xl text-3xl text-green-500" />
                             </div>
                         </Card>
                     </div>
@@ -24,10 +28,10 @@ export const CountChat = ({ record }) => {
                         <Card onClick={() => Router.push('/product')} className="lg:p-10 md:px-8 pc:p-4 p-6 flex justify-between items-center">
                             <div>
                                 <p className='text-3xl font-bold mb-2'>{record.products}</p>
-                                <p className='text-base'>Total Products</p>
+                                <p className='text-base'>Entrepreneurs</p>
                             </div>
                             <div>
-                                <BiCollection className="md:text-5xl text-4xl text-yellow-500" />
+                                <IoIosPeople className="md:text-5xl text-4xl text-yellow-500" />
                             </div>
                         </Card>
                     </div>
@@ -35,10 +39,10 @@ export const CountChat = ({ record }) => {
                         <Card onClick={() => Router.push('/service')} className="lg:p-10 md:px-8 pc:p-4 p-6 flex justify-between items-center">
                             <div>
                                 <p className='text-3xl font-bold mb-2'>{record.services}</p>
-                                <p className='text-base'>Total Services</p>
+                                <p className='text-base'>Events</p>
                             </div>
                             <div>
-                                <MdBatchPrediction className="md:text-5xl text-4xl text-indigo-500" />
+                                <MdOutlineEventSeat className="md:text-5xl text-4xl text-indigo-500" />
                             </div>
                         </Card>
                     </div>
@@ -46,10 +50,10 @@ export const CountChat = ({ record }) => {
                         <Card onClick={() => Router.push('/staff')} className="lg:p-10 md:px-8 pc:p-4 p-6 flex justify-between items-center">
                             <div>
                                 <p className='text-3xl font-bold mb-2'>{record.staffs}</p>
-                                <p className='text-base'>Total Staff</p>
+                                <p className='text-base'>Institutions</p>
                             </div>
                             <div>
-                                <FaRegIdBadge className="md:text-5xl text-3xl text-cyan-500" />
+                                <FaSchool className="md:text-5xl text-3xl text-cyan-500" />
                             </div>
                         </Card>
                     </div>
