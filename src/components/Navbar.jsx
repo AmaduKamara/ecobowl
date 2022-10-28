53dfdad63dbcd52c1062cf34262c7450c4219766
Amkam <amaduskamara@gmail.com>
2022-10-28 11:05:15 +0000

    Created pages and add bg color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <header className="py-5 bg-white shadow-md">
       <nav className="container mx-auto  flex justify-between items-center">
-        <h1>ECO-BOWL</h1>
+        <h1 className="text-[#0072BB] font-semibold text-xl">ECO-BOWL</h1>
         <ul className="flex items-center">
           <li>Login</li>
           <li className="ml-5">Signup</li>
