59eed4e2809992d8d8aad786fdbd42866ff4aab4
Amkam <amaduskamara@gmail.com>
2022-10-28 13:58:06 +0000

    Create event registration page

---
+++ pages/event-registration.tsx
@@ -0,0 +1,14 @@
+const EventRegistration = () => {
+  return (
+    <section>
+      <div className="flex justify-center items-center">
+        <div className="w-3/5"></div>
+        <div className="w-2/5">
+          <h1>Registration</h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default EventRegistration;
