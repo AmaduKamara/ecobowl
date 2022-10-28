a6492cd03289cf26deaec89a8394510c5f440c32
Ishmael Kargbo <ishmaelkargbo5@outlook.com>
2022-10-28 12:52:43 +0000

    Create resend-token.tsx

---
+++ pages/resend-token.tsx
@@ -0,0 +1,12 @@
+import { ResendToken } from "../components/ui/ResendMail";
+
+const Resend = () => {
+   
+    return (
+        <div className="flex h-screen items-center justify-center">
+            <ResendToken />
+        </div>
+    );
+};
+
+export default Resend;
