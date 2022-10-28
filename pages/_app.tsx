1d061c94219cc1d3c3f794648c37a8297e7e4f88
Ishmael Kargbo <ishmaelkargbo5@outlook.com>
2022-10-28 12:52:18 +0000

    Create _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,45 @@
+import 'antd/dist/antd.css';
+import '../styles/globals.css';
+
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { wrapper } from "../redux";
+import { AuthProvider, ProtectRoute } from '../contexts/auth';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { RouteLoader } from '../components/ui/Loading';
+import { Permission } from '../contexts/permission';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const handleStart = (url) => {
+      url !== router.pathname ? setLoading(true) : setLoading(false);
+    };
+    const handleComplete = () => setLoading(false);
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+  }, [router]);
+
+  return <>
+      {/* <AuthProvider>
+        <ProtectRoute>
+          <Permission> */}
+          <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+          </Head>
+          <RouteLoader loading={loading} />
+          <Component {...pageProps} />
+          {/* </Permission>
+        </ProtectRoute>
+      </AuthProvider> */}
+  </>
+}
+
+
+export default wrapper.withRedux(MyApp);
